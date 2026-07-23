27baf8e8da8bc1bba3334357936d2269d938b677
AhmedSa254 <ahmed15prog@gmail.com>
2026-07-23 16:38:05 +0300

    fix migration file

--- Backend/alembic/versions/f1a2b3c4d5e6_add_active_to_departments.py
+++ Backend/alembic/versions/f1a2b3c4d5e6_add_active_to_departments.py
@@ -16,7 +16,7 @@ from sqlalchemy.dialects import mysql
 revision: str = "f1a2b3c4d5e6"
 down_revision: Union[str, Sequence[str], None] = "e63dcd331ce5"
 branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, S    equence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
